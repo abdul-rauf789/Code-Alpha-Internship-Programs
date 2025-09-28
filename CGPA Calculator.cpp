903621e031d5b88b60d52dcf5c65b96f4349c74d
abdul-rauf789 <abdulrauf.123674@gmail.com>
2025-09-28 20:57:19 +0500

    Update CGPA Calculator.cpp

--- CGPA Calculator.cpp
+++ CGPA Calculator.cpp
@@ -1,147 +1,95 @@
-//============================================================================
-// Name        : CGPA.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
-#include <cstdio>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <unordered_map>
+
 using namespace std;
 
-void allocateVar(string*& p,float*& point,string*& sub_name,int*& credits,int n) {
-	p = new string[n];
-	point = new float[n];
-	sub_name = new string[n];
-	credits = new int[n]; // 3 EXTRA for IN-CASE
-
+// Map letter grades to grade points
+double gradeToPoint(const string &grade) {
+    static const unordered_map<string,double> scale{
+        {"A+",4.0},{"A",4.0},{"A-",3.67},
+        {"B+",3.33},{"B",3.0},{"B-",2.67},
+        {"C+",2.33},{"C",2.0},{"C-",1.67},
+        {"D+",1.33},{"D",1.0},{"F",0.0}
+    };
+    auto it = scale.find(grade);
+    return it!=scale.end() ? it->second : -1.0;
 }
 
-void inputs(string* subname,int* credit,string* ptr ,int n) {
-	                     // COURSES ENTRY
-	cout << "~~~Please Enter your Courses Name~~~"<< endl << "Use '-' instead of Space" << endl;
-	for(int i=0;i<n;i++) {
-		cout << "Course " << i+1 << " : ";
-		cin >> subname[i];
-		cout << endl;
-	}
+struct Course {
+    string name;
+    int credit{};
+    string grade;
+    double points{};
+};
 
-	                     //CREDIT Hrs. ENTRY
-
-	cout << "~~~Great. Now, enter your Credits Hrs in your respective Courses~~~" << endl;
-	for(int i=0;i<n;i++) {
-		cout << "Number of Credit Hrs of " << subname[i] << " : ";
-		cin >> credit[i];
-	}
-
-	                    //GRADES ENTRY
-
-	cout << "~~~Enter your Grade in Course (e.g. A+,A,A-,...,D+,D and F~~~" << endl;
-
-	for(int i=0;i<n;i++) {
-		cout << "Enter your Grade in " << subname[i] << " : ";
-		cin >> ptr[i];
-		if(ptr[i] == "A+" || ptr[i] == "A" || ptr[i] == "A-" || ptr[i] == "B+" || ptr[i] == "B-" || ptr[i] == "B" || ptr[i] == "C+" || ptr[i] == "C-" || ptr[i] == "C" || ptr[i] == "D+" || ptr[i] == "D" || ptr[i] == "F" ){
-
-		}
-		else {
-			cout << endl << "!!! Grade, you entered, is not True. Enter Again !!!" << endl;
-			i--;
-
-		}
-	}
-
-}
-
-
-void assignPoints(string* ptr,float* points ,int n) {
-	for(int i=0;i<n;i++) {
-		if(ptr[i] == "A" || ptr[i] == "A+")
-			points[i] = 4.00;
-		else if(ptr[i] == "A-")
-			points[i] = 3.67;
-		else if(ptr[i] == "B+")
-			points[i] = 3.33;
-		else if(ptr[i] == "B")
-			points[i] = 3.00;
-		else if(ptr[i] == "B-")
-			points[i] = 2.67;
-		else if(ptr[i] == "C+")
-			points[i] = 2.33;
-		else if(ptr[i] == "C")
-			points[i] = 2.00;
-		else if(ptr[i] == "C-")
-			points[i] = 1.67;
-		else if(ptr[i] == "D+")
-			points[i] = 1.33;
-		else if(ptr[i] == "D")
-			points[i] = 1.00;
-		else if(ptr[i] == "F")
-			points[i] = 0.00;
-
-	}
-
-}
-
-
-void calculations(float* points,int* credit,float& sum,int& tcredit,int n) {
-	for(int i=0;i<n;i++) {
-		sum += points[i] * credit[i];
-		tcredit += credit[i];
-	}
-}
-
-void outputs(string* subname,float* points,string* ptr,float sum,int tcredit,long double& gpa,int n)  {
-	for(int i=0;i<n;i++) {
-		cout << i+1 << ".  " << subname[i] << " \t " << ptr[i];
-		printf("\t %.2f",points[i]);
-		cout << endl;
-	}
-	cout << endl << endl << endl << "~~~~~\t\t\t    SEMESTER REPORT   \t\t\t~~~~~" << endl;
-	gpa = sum/tcredit;
-	printf("\nGPA  :  %.2Lf",gpa);
-
-}
-
-void deallocate( string* ptr,float*	points,string* subname,int* credit) {
-	delete[] points;
-	delete[] subname;
-	delete[] ptr;
-	delete[] credit;
-
+double semesterGPA(vector<Course>& courses) {
+    double totalPoints = 0.0;
+    int totalCredits = 0;
+    for (auto &c : courses) {
+        totalPoints += c.points * c.credit;
+        totalCredits += c.credit;
+    }
+    return totalCredits ? totalPoints / totalCredits : 0.0;
 }
 
 int main() {
+    cout << fixed << setprecision(2);
+    int totalSemesters;
+    cout << "Enter total number of semesters: ";
+    cin >> totalSemesters;
 
-	int n,tcredit,snum;
-	float sum=0,count=0;
-	long double gpa=0,cgpa=0;
-	string* ptr;
-    float*	points;
-    string* subname;
-    int* credit;
-    cout << "Enter your Total Semesters : ";
-    cin >> snum;
+    double cumulativeGPA = 0.0;
 
-	for(int i=0;i<snum;i++) {
+    for (int s = 1; s <= totalSemesters; ++s) {
+        int nCourses;
+        cout << "\n--- Semester " << s << " ---\n";
+        cout << "Enter number of courses: ";
+        cin >> nCourses;
 
-	gpa=0;
-	count++;
-	cout << "Enter your number of Courses for Semester " << i+1 << " : ";
-	cin >> n;
-	allocateVar(ptr,points,subname,credit,n);
-	inputs(subname,credit,ptr,n);
-	assignPoints(ptr,points,n);
-	calculations(points,credit,sum,tcredit,n);
-	outputs(subname,points,ptr,sum,tcredit,gpa,n);
-	cgpa+=gpa;
-	deallocate(ptr,points,subname,credit);
-	cout << endl;
+        vector<Course> courses(nCourses);
 
-	}
+        // Input courses
+        for (int i = 0; i < nCourses; ++i) {
+            cout << "\nCourse " << i+1 << " name (use - for spaces): ";
+            cin >> courses[i].name;
+            cout << "Credit hours: ";
+            cin >> courses[i].credit;
 
-    cout  << endl << endl << "~~~~~\t\t\tOVERALL SEMESTERS REPORT\t\t\t~~~~~";
-    printf("\nCGPA : %.2Lf",cgpa/count);
-	return 0;
+            // Validate grade input
+            while (true) {
+                cout << "Grade (A+,A,A-,B+,...,F): ";
+                cin >> courses[i].grade;
+                double p = gradeToPoint(courses[i].grade);
+                if (p >= 0) {
+                    courses[i].points = p;
+                    break;
+                }
+                cout << "Invalid grade. Try again.\n";
+            }
+        }
+
+        // Semester report
+        double gpa = semesterGPA(courses);
+        cout << "\nSemester " << s << " Report\n";
+        cout << "-------------------------------------------\n";
+        cout << left << setw(15) << "Course" 
+             << setw(10) << "Grade" 
+             << setw(10) << "Points" << '\n';
+
+        for (auto &c : courses) {
+            cout << left << setw(15) << c.name 
+                 << setw(10) << c.grade 
+                 << setw(10) << c.points << '\n';
+        }
+        cout << "Semester GPA: " << gpa << "\n";
+        cumulativeGPA += gpa;
+    }
+
+    cout << "\n===========================================\n";
+    cout << "Overall CGPA: " << (totalSemesters ? cumulativeGPA/totalSemesters : 0.0) << "\n";
+    cout << "===========================================\n";
+    return 0;
 }
